d26f9d5a578ff5f064337021cdb6bbe353a54a21
Atharva Kulkarni <38763219+IronVenom@users.noreply.github.com>
2018-11-15 12:18:41 +0530

    Update Untitled2.m

--- Untitled2.m
+++ Untitled2.m
@@ -1,8 +1,9 @@
-clc;close all;
+clc;
+close all;
 %-------------------------------------------------------------------------%
-    %first we converted imported table to an array and then rejected unknown
-    %values in some it was depicted by ?
-    %these are unit convension of variable we used through vizier
+    %First we converted imported table to an array and then rejected unknown
+    %values, depicted by ? symbol as shown below.
+    %These are unit convension of variable we used through vizier.org
     %J/A+A/558/A53 Milky Way global survey of star clusters. II. (Kharchenko+, 2013)
     % RAhour F13.8  h     Right Ascension J2000.0, epoch 2000.0
     % DEdeg F13.7  deg    Declination J2000.0, epoch 2000.0
@@ -15,6 +16,8 @@ clc;close all;
     % e_RV F7.2   km/s   ?=-9.99 Error of RV
     % Rcl F8.4    deg    Angular distance from cluster centre
 %-------------------------------------------------------------------------%
+
+%Importing of Data:-
 %b=table2array(b);
 %ra=table2array(ra);
 %dec=table2array(dec);
@@ -28,9 +31,11 @@ clc;close all;
 %pmdec(pmdec==9999.90)=[];
 %e_pm(e_pm==-9.99)=[];
 %rcl(rcl==-1)=[];
-b1=zeros(size(b));%B_mag
-v1=zeros(size(b));%V_mag
-%to eleminate unknown values 
+
+b1=zeros(size(b));%B Magnitude.
+v1=zeros(size(b));%V Magnitude.
+
+%Elimination of unknown values:-
 for i=1:length(b)
     if v(i)~=-99.990 && b(i)~=-99.990
         b1(i)=b(i);
@@ -41,63 +46,76 @@ for i=1:length(b)
     end
 end
 v1(v1==-99)=[];b1(b1==-99)=[];
-b_v=b1-v1;%(B-V)mag
-%For plotting CMD 
-scatter(b_v,v1,'.');title('CM Diagram');xlabel('(B-V)mag');ylabel('Mv(mag)'); %Color Magnitude plot 
+b_v=b1-v1;%(B-V) Magnitude.
+
+%For plotting CMD (Color Magnitude Diagram):-
+scatter(b_v,v1,'.');
+title('CM Diagram');
+xlabel('(B-V)mag');
+ylabel('Mv(mag)'); %Color Magnitude plot 
 figure;
+
 %--------------------------------------------------------------------------%
-    %if mass of stars was given then alternate method is using mass luminosity
-    %relation and using Mo as mass of sun;
+    %If mass of stars is given then we can use alternate method, involving
+    %mass-luminosity relation and using Mo as mass of sun.
     %l=zeros(size(v1));%luminosity here it is ratio of L/Lo where Lo is stellar luminosity 
-    %using mass luminosity relation given in wikki if mass is given 
+    %Using mass luminosity relation (given in Wikipedia and other sources) if mass is given, 
     %for i=1:length(v1)
-     %   if v1(i)<0.43*Mo
-       %     l(i)=0.23*(v1(i)/Mo)^2.3;
-     %   elseif 0.43*Mo<v1(i)<2*Mo
-     %   elseif 2*Mo<v1(i)<20*Mo
-      %      l(i)=1.4*(v1(i)/Mo)^3.5;
-       % elseif 20*Mo<v1(i)<55*Mo
-        %    l(i)=(v1(i)/Mo)^3.5;
-        %else
-         %  l(i)=3200*(v1(i)/Mo);
-        %end
+    %   if v1(i)<0.43*Mo
+    %       l(i)=0.23*(v1(i)/Mo)^2.3;
+    %   elseif 0.43*Mo<v1(i)<2*Mo
+    %   elseif 2*Mo<v1(i)<20*Mo
+    %       l(i)=1.4*(v1(i)/Mo)^3.5;
+    %   elseif 20*Mo<v1(i)<55*Mo
+    %       l(i)=(v1(i)/Mo)^3.5;
+    %   else
+    %       l(i)=3200*(v1(i)/Mo);
+    %   end
     %end
-    %but here we have magnitude in visual band which measure of luminosity so
+    %But here, since we have magnitude in visual band which is a measure of luminosity, so
     %we just plotted directly Mv vs (B-V);
 %--------------------------------------------------------------------------%
+
 %--------------------------------------------------------------------------%
-    %now using right accession and declination we can get direct view of  how
-    %cluster is spread in space and thus to know stars distribution we also
-    %plot contours of predicted distribution using its mean and variance
-    %similarly can be done with proper motion right accession and proper motion
-    %declination to get an idea that what is ideal boundary of the cluster
+    %Now using right accession and declination we can get direct view of  how
+    %cluster is spread in space, and thus to know stars distribution we also
+    %plot contours of predicted distribution using its mean and variance.
+    %Similarly, the same can be done with proper motion right accession and proper motion
+    %declination to get an idea of what is ideal boundary of the cluster
 %--------------------------------------------------------------------------%
-mu=[mean(ra),mean(dec)];Sigma=[var(ra) var(dec)];
+
+mu=[mean(ra),mean(dec)];
+Sigma=[var(ra) var(dec)];
 x1 = 0.1310:0.0125:0.6707; x2 = -73.3264:0.025:-70.8334;
 [X1,X2] = meshgrid(x1,x2);
 F = mvnpdf([X1(:) X2(:)],mu,Sigma);
 F = reshape(F,length(x2),length(x1));
 mvncdf([0 0],[1 1],mu,Sigma);
-scatter(ra, dec,1);
+scatter(ra, dec,1);%Scatter plot showing disctribution.
 hold on;
 contour(x1,x2,F);title('RA vs DEC');ylabel('RA(in h)');xlabel('DEC(in deg)');
 hold off;
 figure;
-surf(x1,x2,F);title('predicted distribution function of stars');ylabel('RA');xlabel('DEC');figure;%normal distribution of stars
+surf(x1,x2,F);title('Predicted distribution function of stars');
+ylabel('RA');xlabel('DEC');figure;%Normal distribution of stars
 %PM_RA vs PM_DEC of stars based on proper motion 
 mu=[mean(pmra,1),mean(pmdec,1)];Sigma=[var(pmra,1) var(pmdec,1)];
 x1 = -377.5700:2:466.5700; x2 = -312.2000:4:412.7300;
 [X1,X2] = meshgrid(x1,x2);
 F = mvnpdf([X1(:) X2(:)],mu,Sigma);
 F = reshape(F,length(x2),length(x1));
-mvncdf([0 0],[1 1],mu,Sigma);  %to find Cummulative density function and drawing countour on proper motion RA vs proper motion DEC plot.
+mvncdf([0 0],[1 1],mu,Sigma);%To find Cummulative density function and drawing countour on proper motion RA vs proper motion DEC plot.
 scatter(pmra,pmdec,1);  
 hold on;
 contour(x1,x2,F);title('pmRA vs pmDEC');ylabel('pmRA(in mas/yr)');xlabel('pmDEC(in mas/yr)');
 hold off;
 figure;
-surf(x1,x2,F);title('predicted distribution function of stars according to proper motion');ylabel('pmRA');xlabel('pmDEC');figure; %normal distribution of stars based on proper motion 
-subplot(2,1,1);plot(rcl,pmra,'.');xlabel('rcl(deg)');ylabel('pmra mas/yr');subplot(2,1,2);plot(rcl,pmdec,'.');xlabel('rcl(deg)');ylabel('pmdec mas/yr');%plot of pmra vs angular distance of stars from cluster's center and pmdec vs angular distance  of stars from cluster's center
+surf(x1,x2,F);title('Predicted distribution function of stars according to proper motion');
+ylabel('pmRA');xlabel('pmDEC');figure; %normal distribution of stars based on proper motion 
+subplot(2,1,1);plot(rcl,pmra,'.');
+xlabel('rcl(deg)');ylabel('pmRA mas/yr');
+subplot(2,1,2);plot(rcl,pmdec,'.');
+xlabel('rcl(deg)');ylabel('pmDEC mas/yr');%plot of pmra vs angular distance of stars from cluster's center and pmdec vs angular distance  of stars from cluster's center
 x=[];y=[];
 e_xy=[];X=[];Y=[];
 for i=1:length(rcl)
